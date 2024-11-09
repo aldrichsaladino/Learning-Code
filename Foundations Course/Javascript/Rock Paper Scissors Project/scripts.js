58819b842eff99e526f32fe1e3ff332d35b0a321
Aldrich Saladino <aldrich.saladino@gmail.com>
2024-11-08 22:13:01 -0700

    trying a different model approach to better create clean code

---
+++ Foundations Course/Javascript/Rock Paper Scissors Project/scripts.js
@@ -0,0 +1,43 @@
+/* The goal is to play rock paper scissors. The following needs to occur:
+1. Logic for a computer choice
+2. Logic for the human choice
+3. Declare the score variables
+4. Add logic for multiple rounds
+5. Add logic to repeat the rounds to 3
+*/
+
+
+// Initialize scores and the round counter
+let playerScore = 0;
+let cpuScore = 0;
+let roundsPlayed = 0;
+
+
+// create a function for the computer choice
+
+
+
+// function to play a single round
+
+
+
+
+    // if switch statements to determine the winner vs loser
+
+
+    // 6. LATER - update the rounds tht are played and then the score
+
+    // LATER - confirm the # of rounds played
+
+
+//5.  function to update the round to the HTML
+
+
+
+// 5. function to update the scoreboard result to HTML
+
+
+
+// 7. function to then display the total score after round 5
+
+// 8. Function to restart the game --> link it to a restart button? Need to reset the score, and hide modal and udpate scoreboard?
