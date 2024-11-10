39c0492d2b3a0ef836d44b8506c84744a123533f
Aldrich Saladino <aldrich.saladino@gmail.com>
2024-11-09 18:07:48 -0700

    new additions

--- Foundations Course/Javascript/Rock Paper Scissors Project/scripts.js
+++ Foundations Course/Javascript/Rock Paper Scissors Project/scripts.js
@@ -14,16 +14,33 @@ let roundsPlayed = 0;
 
 
 // create a function for the computer choice
-
+function getCPUChoice() {
+    const Choices = ['rock', 'paper', 'scissors']
+    const randomIndex = Math.floor(Math.random() * Choices.length) // this pulls random number by length which is 3
+    return Choices[randomIndex];
+}
 
 
 // function to play a single round
-
-
-
+function playRound (playerSelection) {
+    //get the player selection
+    const cpuSelection = getCPUChoice();
+}
 
     // if switch statements to determine the winner vs loser
-
+    if (playerSelection === cpuSelection) {
+        updateRoundResult(`It's a tie! Both players chose ${playerSelection}`)
+    } else if (
+        (playerSelection === "rock" && cpuSelection === "scissors") ||
+        (playerSelection === "paper" && cpuSelection === "rock") ||
+        (playerSelection === "rock" && cpuSelection === "scissors")
+    ) {
+        humanScore++;
+        updateRoundResult (`You win! ${playerSelection} beats ${cpuSelection}`)
+    } else (
+        computerScore++;
+        updateRoundResult(`You Lost! ${playerSelection} loses to ${cpuSelection}`)
+    )
 
     // 6. LATER - update the rounds tht are played and then the score
 
